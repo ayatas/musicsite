de1b07566c2c931c1086113308ef795efad71a4f
ayatas <ayatasdev@gmail.com>
2014-08-06 10:49:21 +0530

    commit controllers 05/08/2014 work

--- protected/controllers/admin/SiteController.php
+++ protected/controllers/admin/SiteController.php
@@ -88,7 +88,7 @@ class SiteController extends BackEndController
 		if (isset($_POST['LoginForm'])) {
 			$model->attributes = $_POST['LoginForm'];
 			// validate user input and redirect to the previous page if valid
-			if ($model->validate() && $model->login())
+			if ($model->login())
 				$this->redirect(Yii::app()->user->returnUrl);
 		}
 		// display the login form
--- protected/controllers/front/ArtistController.php
+++ protected/controllers/front/ArtistController.php
@@ -10,41 +10,50 @@ class ArtistController extends Controller
 
 	public function actionSignup()
 	{
-		$model = new Artist;
-		if(isset($_POST['Artist'])){
-			$model->attributes = $_POST['Artist'];
-			if($model->save()){
+		$model = new User('create');
+		$model1 =  new Artist;
+		if(isset($_POST['User'])){
+			$model->attributes = $_POST['User'];
+			if($model->validate())
+			{
+				$model->password = $this->passwordEncode($_POST['User']['password']);
+				$model->save();
 				$this->redirect(array('artist/signup'));
-			}			
+			}
 		}
-		$this->render('signup', array('model' => $model));
-
+		$this->render('signup', array('model' => $model,'model1' => $model1));
 	}
-
-	// Uncomment the following methods and override them if needed
-	/*
-	 public function filters()
-	 {
-	 // return the filter configuration for this controller, e.g.:
-	 return array(
-	 'inlineFilterName',
-	 array(
-	 'class'=>'path.to.FilterClass',
-	 'propertyName'=>'propertyValue',
-	 ),
-	 );
-	 }
-
-	 public function actions()
-	 {
-	 // return external action classes, e.g.:
-	 return array(
-	 'action1'=>'path.to.ActionClass',
-	 'action2'=>array(
-	 'class'=>'path.to.AnotherActionClass',
-	 'propertyName'=>'propertyValue',
-	 ),
-	 );
-	 }
-	 */
+	public function actionLogin()
+	{
+		$model = new User('login');
+		
+		// if it is ajax validation request
+		if (isset($_POST['ajax']) && $_POST['ajax'] === 'login-form') {
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+		
+		// collect user input data
+		if (isset($_POST['User'])) {
+			$model->attributes = $_POST['User'];
+				// validate user input and redirect to the previous page if valid
+				if ($model->validate() && $model->login())
+				{
+					echo "hello";
+					exit;
+					$this->redirect('account');
+				}
+		}
+		
+		$this->render('login', array('model' => $model));	
+	}
+	public function passwordEncode($password)
+	{
+		return base64_encode(base64_encode($password));
+	}
+	
+	public function actionAccount()
+	{
+		echo "Hello";
+	}
 }
